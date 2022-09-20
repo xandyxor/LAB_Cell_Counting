0a6e38045b9c82bebf3e8d8ceab138ec4695eb28
xandyxor <andy112247@gmail.com>
2022-09-21 03:44:32 +0800

    plt to opencv

--- main.py
+++ main.py
@@ -65,23 +65,36 @@ for i in range(blobs_log.shape[0]):
 # fig, ax = plt.subplots(figsize=(20, 20), sharex=True, sharey=True)
 cv2.imshow('output1img',output1img)
 cv2.waitKey(0)
-output2img = gray.copy()
+# output2img = gray.copy()
+gray_three_channel = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
 # ax.imshow(gray, interpolation='nearest',cmap="gray")
+shapes = np.zeros_like(gray_three_channel, np.uint8)
+
 for i in range(blobs_log.shape[0]):
     y, x, r = blobs_log [i,]
     # ax.text(x,y, str(i), fontdict=font)
 
-    if r > 1.5:
+    # # Draw shapes
+    # cv2.rectangle(shapes, (5, 5), (100, 75), (255, 255, 255), cv2.FILLED)
+    # cv2.circle(shapes, (300, 300), 75, (255, 255, 255), cv2.FILLED)
+
+    # Generate output by blending image with shapes image, using the shapes
+    # images also as mask to limit the blending to those parts
+  
+
+    if r > 0:
         # c = plt.Circle((x, y), r, color="red", alpha = 0.4, linewidth=2, fill=True)
-        cv2.circle(output2img,(int(x), int(y)), int(r), color=(0,0,0), thickness=2)
+        cv2.circle(shapes,(int(x), int(y)), int(r), color=(0,0,255), thickness=-1)
 
         # ax.add_patch(c)
-    else:
-        # c = plt.Circle((x, y), r, color="cyan", linewidth=2, fill=False)
-        cv2.circle(output2img,(int(x), int(y)), int(r), color=(0,255,0), thickness=-1)
-
-        # ax.add_patch(c)
-
-cv2.imshow('output2img',output2img)
+    # else:
+    #     # c = plt.Circle((x, y), r, color="cyan", linewidth=2, fill=False)
+    #     cv2.circle(gray_three_channel,(int(x), int(y)), int(r), color=(0,0,255), thickness=-1)
+    #     # ax.add_patch(c)
+out = gray_three_channel.copy()
+alpha = 0.2
+mask = shapes.astype(bool)
+out[mask] = cv2.addWeighted(gray_three_channel, alpha, shapes, 1 - alpha, 0)[mask]
+cv2.imshow('out',out)
 cv2.waitKey(0)
 print(blobs_log.shape [0])
